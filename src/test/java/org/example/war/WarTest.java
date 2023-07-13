5ec964af8a30e5d2550b781ef5be364fe89f9f5b
victorteje <vteje2@gmail.com>
2023-07-13 15:19:31 +0200

    starting war tests

--- src/test/java/org/example/war/WarTest.java
+++ src/test/java/org/example/war/WarTest.java
@@ -14,22 +14,22 @@ class WarTest {
     int strength = 150;
     int health = 300;
 
-//    Saxon saxon = new Saxon(health, strength);
-//    Viking viking = new Viking(name, health, strength);
-//
-//    War war = new War();
-//
-//    @Test
-//    public void should_receive_0_arguments() {
-//        assertNotNull(war);
-//    }
-//
-//    @Test
-//    public void should_assign_an_empty_array_to_the_vikingArmy_property() {
-//        List<Viking> vikingArmy = war.getVikingArmy();
-//        assertTrue(vikingArmy.isEmpty());
-//    }
-//
+    Saxon saxon = new Saxon(health, strength);
+    Vikings viking = new Vikings(name, health, strength);
+
+    War war = new War();
+
+    @Test
+    public void should_receive_0_arguments() {
+        assertNotNull(war);
+    }
+
+    @Test
+    public void should_assign_an_empty_array_to_the_vikingArmy_property() {
+        List<Vikings> vikingArmy = war.getVikingArmy();
+        assertTrue(vikingArmy.isEmpty());
+    }
+
 //    @Test
 //    public void should_assign_an_empty_array_to_the_saxonArmy_property() {
 //        List<Object> saxonArmy = war.getSaxonArmy();
