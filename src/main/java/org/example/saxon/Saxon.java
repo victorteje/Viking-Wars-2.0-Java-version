b9c481876d5d0f2a4a9afcf032337f20079c6570
victorteje <vteje2@gmail.com>
2023-07-13 14:27:04 +0200

    finished saxon tests

--- src/main/java/org/example/saxon/Saxon.java
+++ src/main/java/org/example/saxon/Saxon.java
@@ -10,7 +10,16 @@ public class Saxon extends Soldier {
         return strength;
     }
 
+    String msg;
     public void receiveDamage(int damage) {
         this.health = this.health - damage;
+        if (this.health > 0){
+            msg = "A Saxon has received" + damage + "points of damage";
+        }
     }
+
+    public String getMsg() {
+        return msg;
+    }
+
 }
